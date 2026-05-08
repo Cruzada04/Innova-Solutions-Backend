15575303a5d089119fc75ed1576758990b8746d8
Vasco B Goode <vascomartinez9@gmail.com>
2026-05-07 23:33:24 -0500

    Add validation annotations to DTO fields

--- src/main/java/com/upc/innovasolutionsbackend/dtos/RelacionTutorEstudianteRequestDTO.java
+++ src/main/java/com/upc/innovasolutionsbackend/dtos/RelacionTutorEstudianteRequestDTO.java
@@ -1,5 +1,6 @@
 package com.upc.innovasolutionsbackend.dtos;
 
+import jakarta.validation.constraints.NotNull; //
 import lombok.*;
 
 @Getter
@@ -7,6 +8,10 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RelacionTutorEstudianteRequestDTO {
+
+    @NotNull(message = "El ID del tutor es obligatorio") //
     private Long tutorId;
+
+    @NotNull(message = "El ID del estudiante es obligatorio") //
     private Long estudianteId;
 }
