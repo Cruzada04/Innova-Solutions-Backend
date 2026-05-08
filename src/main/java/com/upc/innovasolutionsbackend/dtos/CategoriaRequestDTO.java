f13393a92ae86ac4b952ac1749451427d8620f79
Vasco B Goode <vascomartinez9@gmail.com>
2026-05-07 20:42:41 -0500

    Add validation annotations to CategoriaRequestDTO

--- src/main/java/com/upc/innovasolutionsbackend/dtos/CategoriaRequestDTO.java
+++ src/main/java/com/upc/innovasolutionsbackend/dtos/CategoriaRequestDTO.java
@@ -1,5 +1,7 @@
 package com.upc.innovasolutionsbackend.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -7,6 +9,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoriaRequestDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
     private String nombre;
+
+    @NotBlank(message = "La URL del icono es obligatoria")
     private String iconoUrl;
 }
