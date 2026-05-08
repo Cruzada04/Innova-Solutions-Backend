b4242f8abe19fd53686aff52022cb510fe86e5ab
Vasco B Goode <vascomartinez9@gmail.com>
2026-05-07 21:11:52 -0500

    Add validation annotations to ElementoGuardadoRequestDTO

--- src/main/java/com/upc/innovasolutionsbackend/dtos/ElementoGuardadoRequestDTO.java
+++ src/main/java/com/upc/innovasolutionsbackend/dtos/ElementoGuardadoRequestDTO.java
@@ -1,5 +1,7 @@
 package com.upc.innovasolutionsbackend.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -7,7 +9,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ElementoGuardadoRequestDTO {
+
+    @NotBlank(message = "El tipo de elemento es obligatorio")
     private String tipoElemento;
+
+    @NotNull(message = "El ID del elemento es obligatorio")
     private Long elementoId;
+
+    @NotNull(message = "El ID de usuario es obligatorio")
     private Long usuarioId;
 }
