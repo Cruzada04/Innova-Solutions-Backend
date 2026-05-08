b48888e1b7d3266b2b67341f83e89d56cc1e6609
Vasco B Goode <vascomartinez9@gmail.com>
2026-05-07 23:40:32 -0500

    Add validation annotations to RolRequestDTO

--- src/main/java/com/upc/innovasolutionsbackend/dtos/RolRequestDTO.java
+++ src/main/java/com/upc/innovasolutionsbackend/dtos/RolRequestDTO.java
@@ -1,5 +1,7 @@
 package com.upc.innovasolutionsbackend.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -7,5 +9,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RolRequestDTO {
+
+    @NotBlank(message = "El nombre del rol es obligatorio")
+    @Size(min = 3, max = 30, message = "El nombre del rol debe tener entre 3 y 30 caracteres")
     private String nombre;
 }
